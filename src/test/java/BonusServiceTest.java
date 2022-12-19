ad738da8f775a4d430b31a9580dae774b10748a7
Aleksandra <zharovaaleksis@gmail.com>
2022-12-19 13:18:58 +0300

    all tests done

--- src/test/java/BonusServiceTest.java
+++ src/test/java/BonusServiceTest.java
@@ -59,7 +59,7 @@ public class BonusServiceTest {
         // подготавливаем данные:
         long amount = 1000000;
         boolean registered = false;
-        long expected = 5;
+        long expected = 500;
 
         // вызываем целевой метод:
         long actual = service.calculate(amount, registered);
